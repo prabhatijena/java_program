d3757f2068ad97948c28cf7d4d567049358b91b8
PrabhatiJena <jenaprabhati97@gmail.com>
2025-10-25 13:28:34 +0530

    code modified

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,11 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> words = Arrays.asList("Apple", "banana", "apple", "Orange", "banana", "apple");
-        int n = 2;
-       List<String> count =  words.stream()
-               .map(x->x.toLowerCase())
-               .collect(Collectors.groupingBy(x->x,Collectors.counting()))
-               .entrySet().stream()
-               .filter(x->x.getValue()!=n)
-               .map(x->x.getKey())
-               .collect(Collectors.toList());
-        System.out.println(count);
+        List<String> lists = Arrays.asList("Prabhati","Bipin","Swetapadma","Lipi");
+        List<String> sortedByLength = lists.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+        System.out.println(sortedByLength);
 
     }
 }
